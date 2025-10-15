ef784f1d6848ddea1fa9c15ab1e75458e75ad4c5
Gaaldra <gabrielalves.quadra@gmail.com>
2025-10-14 22:00:59 -0300

    feat: adiciona controlador de cliente

---
+++ Agendamentos.API/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using Agendamentos.API.Database;
+using Agendamentos.Biblioteca;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agendamentos.API.Controllers;
+
+[Route("[controller]")] // Anotação
+[ApiController]
+public class ClientController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult RegisterClient([FromBody]Client request)
+    {
+        return Created();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetClientByID(int id)
+    {
+        return Ok(new
+        {
+            Message = id
+        });
+    }
+}
