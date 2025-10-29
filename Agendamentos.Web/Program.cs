25cde20c21974c193fc32ed1e5867ae2dc437310
Gaaldra <gabrielalves.quadra@gmail.com>
2025-10-29 19:22:17 -0300

    feat: cria o projeto web

--- Agendamentos.Web/Program.cs
+++ Agendamentos.Web/Program.cs
@@ -1,36 +1,36 @@
+using Agendamentos.Web.Components;
+
 namespace Agendamentos.Web
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddRazorPages();
-			builder.Services.AddRazorComponents()
-				.AddInteractiveServerComponents();
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
 
-			var app = builder.Build();
+            var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
 
-			app.UseAuthorization();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
 
-			app.MapRazorPages();
-			
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
